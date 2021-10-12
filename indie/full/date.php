4dcca9808acdf48f0055def5caef0e1d4c101d9e
jbmagination <26027089+jbmagination@users.noreply.github.com>
2021-10-12 17:58:05 -0400

    Potential timelock fix 2

--- indie/full/date.php
+++ indie/full/date.php
@@ -1,8 +1,1 @@
-<?php
-// Copyright 2020 gyroninja
-// This program is licensed under the CC0 1.0 Universal license
-// https://creativecommons.org/publicdomain/zero/1.0/legalcode.txt
-// This is a reimplementation of date.php that
-// returns a UNIX timestamp in seconds
-echo "currentdate=" . (new DateTime())->getTimestamp() . "\n"
-?>
+currentdate=1293235200
